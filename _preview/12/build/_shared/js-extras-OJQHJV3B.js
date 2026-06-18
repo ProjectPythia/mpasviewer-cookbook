5ffc59775ba621ddb671d9753914b43405b63c8c
jhbravo <18407824+jhbravo@users.noreply.github.com>
2026-06-18 20:45:55 +0000

    Delete preview for pull request \#12

--- _preview/12/build/_shared/js-extras-OJQHJV3B.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/mpasviewer-cookbook/_preview/12/build/_shared/chunk-YYQE6CDZ.js";import"/mpasviewer-cookbook/_preview/12/build/_shared/chunk-OZE3FFNP.js";export default a();
--- _preview/12/build/_shared/wren-FGHU2HOT.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/mpasviewer-cookbook/_preview/12/build/_shared/chunk-MOOSZ6D3.js";import"/mpasviewer-cookbook/_preview/12/build/_shared/chunk-OZE3FFNP.js";export default a();
